61e25a6ac86347c737b7103c9609a2d61d6c672c
silpako <silpako167@gmail.com>
2025-04-12 23:04:55 +0530

    recommended for you section

--- lib/views/home/recomented_container.dart
+++ lib/views/home/recomented_container.dart
@@ -7,24 +7,18 @@ class RecomentedContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 250,
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          left: 12,
-          right: 12,
-          top: 8.0,
-          bottom: 8.0,
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+      child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Recommented For You",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                  "Recommended For You",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
                 ),
                 Text(
                   "See all",
@@ -36,13 +30,14 @@ class RecomentedContainer extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 10),
+
+            const SizedBox(height: 10),
 
             SizedBox(
-              height: 180,
+              height: 200,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: [
+                children: const [
                   SingleProduct(),
                   SizedBox(width: 10),
                   SingleProduct(),
@@ -58,8 +53,29 @@ class RecomentedContainer extends StatelessWidget {
   }
 }
 
-class SingleProduct extends StatelessWidget {
+class SingleProduct extends StatefulWidget {
   const SingleProduct({super.key});
+
+  @override
+  State<SingleProduct> createState() => _SingleProductState();
+}
+
+class _SingleProductState extends State<SingleProduct> {
+  int quantity = 1;
+
+  void incrementQuantity() {
+    setState(() {
+      quantity++;
+    });
+  }
+
+  void decrementQuantity() {
+    if (quantity > 1) {
+      setState(() {
+        quantity--;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +90,6 @@ class SingleProduct extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // 🔷 Top section with grey background
             Container(
               decoration: const BoxDecoration(color: Color(0xFFEAEAEA)),
               child: Column(
@@ -82,25 +97,96 @@ class SingleProduct extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Center(
-                        child: Image.asset(
-                          AppAssets.percentageIg,
-                          height: 30,
-                          fit: BoxFit.contain,
-                        ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Image.asset(
+                            AppAssets.percentageIg,
+                            height: 30,
+                            fit: BoxFit.contain,
+                          ),
+                          const Text(
+                            '5%',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.green, width: 1.5),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Center(
-                          child: Icon(Icons.add, size: 16, color: Colors.green),
-                        ),
-                      ),
+                      quantity == 0
+                          ? GestureDetector(
+                            onTap: incrementQuantity,
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.green,
+                                  width: 1.5,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.add,
+                                  size: 16,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ),
+                          )
+                          : Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: Colors.green,
+                                width: 1.5,
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: decrementQuantity,
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 6,
+                                    ),
+                                    child: Icon(
+                                      Icons.remove,
+                                      size: 14,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  quantity.toString().padLeft(2, '0'),
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                    color: Colors.green,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: incrementQuantity,
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 6,
+                                    ),
+                                    child: Icon(
+                                      Icons.add,
+                                      size: 14,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                     ],
                   ),
                   Center(
@@ -113,40 +199,57 @@ class SingleProduct extends StatelessWidget {
                 ],
               ),
             ),
+
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "new image",
-                    style: const TextStyle(fontWeight: FontWeight.w600),
+                  const Text(
+                    "Organic Almond Milk",
+                    style: TextStyle(fontWeight: FontWeight.w600),
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                   ),
                   Row(
-                    children: [
-                      Text(
-                        "200",
-                        style: const TextStyle(
-                          fontSize: 12,
-                          decoration: TextDecoration.lineThrough,
-                          color: Colors.grey,
-                        ),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Row(
+                        children: [
+                          Text(
+                            "200g",
+                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                          ),
+                          SizedBox(width: 4),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 20,
+                            color: Colors.green,
+                          ),
+                        ],
                       ),
-                      const SizedBox(width: 6),
-                      Text(
-                        "150",
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "₹200",
+                            style: TextStyle(
+                              fontSize: 12,
+                              decoration: TextDecoration.lineThrough,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Text(
+                            "₹180",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
-                  ),
-                  Text(
-                    "10",
-                    style: const TextStyle(color: Colors.grey, fontSize: 12),
                   ),
                 ],
               ),
